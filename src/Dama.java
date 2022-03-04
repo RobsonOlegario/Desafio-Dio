ecadaa53a0e91cbdcda4178ee7405a10da55cd4e
RobsonOlegario <96242107+RobsonOlegario@users.noreply.github.com>
2022-03-04 13:51:44 -0300

    Initial commit

---
+++ src/Dama.java
@@ -0,0 +1,33 @@
+public class Dama {
+
+import java.util.Scanner;
+
+
+    public class Main {
+        public static void main(String[] args) {
+            Scanner sc = new Scanner(System.in);
+
+            int x1,y1,x2,y2;
+            //se for na mesma linha ou na mesma coluna ou na mesma diagonal, gasta 1 movimento
+            //se estiver em qualquer outra posição, a rainha gastará 2 movimentos!
+
+            while(true){
+                x1 = sc.nextInt();
+                y1 = sc.nextInt();
+                x2 = sc.nextInt();
+                y2 = sc.nextInt();
+                if(x1 == 0 && y1 == 0 && x2 == 0 && y2 == 0) break; //condição de parada
+                if(x1 == x2 && y1 == y2)		//mesma posição
+                    System.out.println("0");
+                else if(x1 == x2 || y1 == y2)	//mesma linha ou mesma coluna
+                    System.out.println("1");
+                else if((x2-x1)==-(y2-y1) || -(x2-x1)==-(y2-y1) || -(x2-x1)==(y2-y1) || (x2-x1)==(y2-y1))
+                    System.out.println("1");	//mesma diagonal
+                else
+                    System.out.println("2");	//o resto...
+            }
+            sc.close();
+        }
+    }
+
+}
